6f905d0de9452b04810d73eef229870e944a29bb
Abdirahman04 <abdixcom@gmail.com>
2024-01-23 06:28:34 +0300

    added dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import HomeView from "../views/HomeView.vue";
 import SignUpViewVue from "@/views/SignUpView.vue";
 import SignInViewVue from "@/views/SignInView.vue";
 import CommunityViewVue from "@/views/CommunityView.vue";
+import DashboardViewVue from "@/views/DashboardView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", name: "home", component: HomeView },
   { path: "/signup", name: "signup", component: SignUpViewVue },
   { path: "/signin", name: "signin", component: SignInViewVue },
   { path: "/community", name: "community", component: CommunityViewVue },
+  { path: "/dashboard", name: "dashboard", component: DashboardViewVue },
 ];
 
 const router = createRouter({
