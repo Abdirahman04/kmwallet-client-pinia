2f1096fb8356b5060489735086989853594b624c
Abdirahman04 <abdixcom@gmail.com>
2024-01-23 06:27:03 +0300

    added community

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,23 +2,13 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignUpViewVue from "@/views/SignUpView.vue";
 import SignInViewVue from "@/views/SignInView.vue";
+import CommunityViewVue from "@/views/CommunityView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/signup",
-    name: "signup",
-    component: SignUpViewVue,
-  },
-  {
-    path: "/signin",
-    name: "signin",
-    component: SignInViewVue,
-  },
+  { path: "/", name: "home", component: HomeView },
+  { path: "/signup", name: "signup", component: SignUpViewVue },
+  { path: "/signin", name: "signin", component: SignInViewVue },
+  { path: "/community", name: "community", component: CommunityViewVue },
 ];
 
 const router = createRouter({
