fa9e5dc9611cc8d2959023b238883d6293f704e5
Abdirahman04 <abdixcom@gmail.com>
2024-01-23 06:21:30 +0300

    added sign up

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SignUpViewVue from "@/views/SignUpView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUpViewVue,
   },
 ];
 
