7d2509821f06258add0bd1846dc8902b0aa13af1
Abdirahman04 <abdixcom@gmail.com>
2024-01-24 13:33:02 +0300

    added passwordFormInput

--- src/components/PasswordFormInput.vue
+++ src/components/PasswordFormInput.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
 import { defineEmits } from "vue";
+import { ref, computed } from "vue";
+import FormInput from "@/components/FormInput.vue";
 
 const props = defineProps(["modelValue"]);
 const emits = defineEmits(["update:modelValue"]);
+const pass = ref<string>("");
+const visible = ref<boolean>(false);
+const passClass = () => {
+  if (visible.value) return "text";
+  return "password";
+};
 
 const updateModelValue = (event: Event) => {
   const value = (event.target as HTMLInputElement).value;
@@ -12,10 +20,12 @@ const updateModelValue = (event: Event) => {
 </script>
 
 <template>
-  <input
+  <form-input
+    v-model="pass"
     :value="props.modelValue"
     @input="updateModelValue($event)"
     v-bind="$attrs"
-    class="border-2"
-  />
+    :type="passClass"
+  ></form-input>
+  <button @click="visible = !visible">See</button>
 </template>
