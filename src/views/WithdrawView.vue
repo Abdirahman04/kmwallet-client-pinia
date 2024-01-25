a902e95378ae3614733c95c3291ea51920874de1
Abdirahman04 <abdixcom@gmail.com>
2024-01-25 18:44:46 +0300

    added withdrawView

---
+++ src/views/WithdrawView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <the-nav-bar></the-nav-bar>
+    <div class="flex flex-grow flex-col bg-amber-100">
+      <the-sub-nav path="account" text="Withdraw"></the-sub-nav>
+      <div class="flex flex-grow justify-center items-center">
+        <div>
+          <h1>How much do you want to withdraw?</h1>
+          <form-input v-model="balance"></form-input>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import FormInput from "@/components/FormInput.vue";
+import { ref } from "vue";
+
+const balance = ref<string>("");
+</script>
