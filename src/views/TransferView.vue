ef4ad21fe3c22984f4b7c16957fb87beb497395d
Abdirahman04 <abdixcom@gmail.com>
2024-01-25 18:46:32 +0300

    added transferView

---
+++ src/views/TransferView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <the-nav-bar></the-nav-bar>
+    <div class="flex flex-grow flex-col bg-amber-100">
+      <the-sub-nav path="account" text="Transfer"></the-sub-nav>
+      <div class="flex flex-grow justify-center items-center">
+        <div>
+          <h1>How much do you want to transfer?</h1>
+          <form-input v-model="balance"></form-input>
+          <form-input v-model="target"></form-input>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import FormInput from "@/components/FormInput.vue";
+import { ref } from "vue";
+
+const balance = ref<string>("");
+const target = ref<string>("");
+</script>
